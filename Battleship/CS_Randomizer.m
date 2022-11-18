9164d58ad88ddea1126b818430af398c5de87009
nathanasbury <117854483+nathanasbury@users.noreply.github.com>
2022-11-18 14:01:56 -0500

    Randomizer, splashscreen,
    woooooo

---
+++ Battleship/CS_Randomizer.m
@@ -0,0 +1,15 @@
+function [CR] = Randomizer()
+
+%function will return a 7 by 7 of zeros
+CR = zeros(7,7);
+
+for cow_grass_count = 1:21
+    
+    cow_placed =  false;
+    while ~cow_placed
+        grid = randi([1 49]);
+
+
+
+    end
+end
