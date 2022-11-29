42b0914d4699ec66fb1bbcee3b8bee576c4fafeb
nathanasbury <117854483+nathanasbury@users.noreply.github.com>
2022-11-29 14:40:36 -0500

    Updated Randomizer

--- Battleship/CS_Randomizer.m
+++ Battleship/CS_Randomizer.m
@@ -2,16 +2,7 @@ function CR = CS_Randomizer(~)
 
 %function will return a 7 by 7 of zeros and twos
 
-cowCount = zeros(7);
-CR_1 = zeros(7);
-A = randi([0,1],[7,7]);
-
-for k = size(cowCount)
-    b(k) = k/k+1;
-    CR_1(:,:,k) = b(k)*A;
-end
-
-CR = CR_1(:,:,7);
+CR = randi([0,1],[7,7]);
 
 end
 
