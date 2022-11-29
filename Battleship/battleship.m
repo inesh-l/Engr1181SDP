b2dd9962e8c045708a1e775e2e33292e834888a8
nathanasbury <117854483+nathanasbury@users.noreply.github.com>
2022-11-29 17:23:28 -0500

    Working Game for 1 Turn
    The game works for a total of one turn, i need to create the loop that will let multiple turns happen

--- Battleship/battleship.m
+++ Battleship/battleship.m
@@ -17,39 +17,59 @@ drawScene(splashscreen,splashy)
 
 %Initialize Game
 
-sSize = 63;
-zFactor = 5;
+sSize_2 = 63;
+zFactor_2 = 3;
 BGC = [167 177 183];
 
 %WARNING, sprite sheet not completed, game will not run
-Game = simpleGameEngine('PlaceholderSheet',sSize,sSize,zFactor,BGC);
+main_game = simpleGameEngine('SpriteSheet.png',sSize_2,sSize_2,zFactor_2,BGC);
 
 %Game Objects
-blank_sprite = 1;
-grass_sprite = 2;
-cow_grass = 3;
-hit_steak_sprite = 4;
-dirt_sprite = 5;
+grass_sprite = 1;
+cow_grass = 2;
+tall_grass  = 3;
+dirt_sprite = 4;
+hit_steak_sprite = 5;
 
 %Creating the Board
-board = grass_sprite*ones(7,7);
-drawscene(board,Game)
+board_image = grass_sprite*ones(7,7);
+drawScene(main_game,board_image)
 
 %Call function to generate cow placements
-cow = CS_Randomizer;
+board_image2 = CS_Randomizer;
+drawScene(main_game,board_image2)
 
+%Eventually gameplay loop, creating the change that happens on fire
+[r, c] = getMouseInput(main_game);
+
+board_change = board_image2;
+
+while board_change == board_image2
+if board_change(r,c) == 2
+    board_change(r,c) = 5; 
+else 
+    board_change(r,c) = 4;
+end
+drawScene(main_game,board_change)
+end
 % Gameplay loop
+% [click] = getMouseInput(Game);
+% if click = cow_grass
+% 
+% else
+% end
 
+    
 % Create dirt sprite if grass was hit (value of 0)
 
 % Create steak sprite if cow was hit (value of 1)
 
 %Win Screen when all cows hit (placeholder)
-splashscreen = simpleGameEngine('WinScreen.png',sSize,sSize,zFactor,BGC);
-
-win = 1;
-
-drawscene(win,winscreen)
+% splashscreen = simpleGameEngine('WinScreen.png',sSize,sSize,zFactor,BGC);
+% 
+% win = 1;
+% 
+% drawScene(win,winscreen)
 
 
 
