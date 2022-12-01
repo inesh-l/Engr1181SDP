cd8f5d67562f38acb69a06d575775864b6646293
nathanasbury <117854483+nathanasbury@users.noreply.github.com>
2022-12-01 14:28:34 -0500

    Final Edits

--- Battleship/test.m
+++ Battleship/test.m
@@ -24,5 +24,22 @@ clear
 %    end
 %    disp([ x(:) count ]);
 
-[y,Fs] = audioread('MOO.wav');
-sound(y,Fs,16)
+score = 5;
+sSize3 = 255;
+zFactor3 = 5;
+BGC = [167 177 183];
+
+win_screen = simpleGameEngine('Win Screen.png',sSize3,sSize3,zFactor3,BGC);
+
+win = 1;
+%xlabel('YOUR SCORE',score)
+score = 5;
+prompt = fprintf('Your Score is %d\n', score);
+
+drawScene(win_screen,win)
+title("Your Score is " + score);
+%fprintf('Your Score %.2f',score);
+% while ending == 1
+%     clear sound
+%     close all
+% end
